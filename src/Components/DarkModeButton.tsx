03e1d1d35b1e03a0897279749fde172ab2ce3d8a
carl-thilander <carl.thilander@medieinstitutet.se>
2025-02-24 16:36:03 +0100

    updated button

--- src/Components/DarkModeButton.tsx
+++ src/Components/DarkModeButton.tsx
@@ -1,16 +1,26 @@
 // DarkModeButton.tsx
 import React from 'react';
+import styled from 'styled-components';
 
 interface DarkModeButtonProps {
   toggleTheme: () => void;
   isDarkMode: boolean;
 }
 
+const Button = styled.button<{isDarkMode: boolean}>`
+padding: 5px 10px;
+cursor: pointer;
+border: none;
+width: auto;
+height: 30px;
+border-radius: 5px;
+`
+
 const DarkModeButton: React.FC<DarkModeButtonProps> = ({ toggleTheme, isDarkMode }) => {
   return (
-    <button onClick={toggleTheme}>
+    <Button isDarkMode={isDarkMode} onClick={toggleTheme}>
       Switch to {isDarkMode ? 'Light' : 'Dark'} Mode
-    </button>
+    </Button>
   );
 };
 
