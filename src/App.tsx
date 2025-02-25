e79f6ce247bfe020396d59969739708fbbd03859
Carl-Thilandergit <carl.thilander@medieinstitutet.se>
2025-02-25 13:27:14 +0100

    Changed react.fc to arrow function

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import { Outlet } from 'react-router';
-import styled, { ThemeProvider } from 'styled-components';
-import DarkModeButton from './Components/DarkModeButton';
-import { darkTheme, lightTheme } from './Components/theme';
+import { useState } from "react";
+import { Outlet } from "react-router";
+import styled, { ThemeProvider } from "styled-components";
+import DarkModeButton from "./Components/DarkModeButton";
+import { darkTheme, lightTheme } from "./Components/theme";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  color: ${({theme}) => theme.textColor};
+  color: ${({ theme }) => theme.textColor};
   transition: color 0.5s, background-color 0.5s;
   position: relative;
 `;
@@ -16,7 +16,7 @@ const Header = styled.header`
   padding: 8px 16px;
   display: flex;
   justify-content: space-between;
-  background-color: ${({theme}) => theme.background};
+  background-color: ${({ theme }) => theme.background};
   transition: background-color 0.5s;
   position: sticky;
   z-index: 100;
@@ -30,48 +30,46 @@ const MainContent = styled.div`
 `;
 
 const Sidebar = styled.aside`
-background-color: ${({ theme }) => theme.mainBackground};
-transition: background-color 0.5s;
+  background-color: ${({ theme }) => theme.mainBackground};
+  transition: background-color 0.5s;
   padding: 16px;
   border-radius: 8px;
-  color: ${({theme}) => theme.textColor};
+  color: ${({ theme }) => theme.textColor};
 `;
 
 const Main = styled.main`
-background-color: ${({ theme }) => theme.mainBackground};
-transition: background-color 0.5s;
+  background-color: ${({ theme }) => theme.mainBackground};
+  transition: background-color 0.5s;
   padding: 16px;
   border-radius: 8px;
   flex: 1;
 `;
 
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-const App: React.FC = () => {
-  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+  const toggleTheme = () => setIsDarkMode((prev) => !prev);
 
-  const toggleTheme = () => setIsDarkMode(!isDarkMode);
   return (
     <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-    <Container>
-      <Header>
-        <h1>Bread Page</h1>
-        <span>Right</span>
-        <span>Right</span>
-        <span>Right</span>
-        <DarkModeButton toggleTheme={toggleTheme} isDarkMode={isDarkMode}/>
-      </Header>
+      <Container>
+        <Header>
+          <h1>Bread Page</h1>
+          <span>Right</span>
+          <span>Right</span>
+          <span>Right</span>
+          <DarkModeButton toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
+        </Header>
 
-      <MainContent>
-        <Sidebar>Bread-list maybe?</Sidebar>
-        <Main>
-          <Outlet /> {/* Detta ändras beroende på URL */}
-        </Main>
-      </MainContent>
-    </Container>
+        <MainContent>
+          <Sidebar>Bread-list maybe?</Sidebar>
+          <Main>
+            <Outlet /> {/* Detta ändras beroende på URL */}
+          </Main>
+        </MainContent>
+      </Container>
     </ThemeProvider>
   );
 };
 
 export default App;
-
-
